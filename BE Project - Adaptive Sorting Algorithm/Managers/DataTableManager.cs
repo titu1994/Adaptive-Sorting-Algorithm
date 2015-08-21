207d19a7a4ade7187fbcf3a5ee85ab0259696e7f
Somshubra Majumdar <titu1994@gmail.com>
2015-08-21 20:56:24 +0530

    Created JsonManager to manage the json results obtained from Java.
    Created DataTableManager to manage the Results DataTable and also manage the DecisionTree for ML

---
+++ BE Project - Adaptive Sorting Algorithm/Managers/DataTableManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.MachineLearning.DecisionTrees.Learning;
+using Accord.Math;
+using Accord.Statistics.Filters;
+
+namespace BE_Project___Adaptive_Sorting_Algorithm.Managers
+{
+    class DataTableManager
+    {
+        // Original Data table
+        public DataTable Table { get; }
+        // Symbol table generated from Data table
+        public DataTable Symbols { get; set; }
+
+        // Codified Input[] vector
+        public double[][] Inputs { get; set; }
+        // Codified Output vector
+        public int[] Outputs { get; set; }
+
+        private DecisionTree tree;
+        private C45Learning c45;
+
+        // Labels for the DataTable
+        private string[] inputColumns =
+        {
+            "Array Size", "Runs", "Array Type", "Insertion Sort", "Shell Sort",
+            "Heap Sort", "Merge Sort", "Quick Sort", "Parallel Merge Sort"
+        };
+        private string outputColumn = "Selected Sorting Algorithm";
+
+        private Codification codebook;
+
+        public DataTableManager()
+        {
+            Table = new DataTable("Sorting Table");
+            foreach (string t in inputColumns)
+                Table.Columns.Add(t);
+            Table.Columns.Add(outputColumn);
+        }
+
+        // Add each result one by one
+        public void AddResult(JsonManager.Result r)
+        {
+            object[] sr =
+            {
+                r.arraySize + "", r.runs + "", r.arraytype, r.insertionSortExecutionTime + "",
+                r.shellSortExecutionTime + "", r.heapSortExecutionTime + "", r.mergeSortExecutionTime + "",
+                r.quickSortExecutionTime + "", r.parallelMergeSortExecutionTime + "", r.bestClass
+            };
+            Table.Rows.Add(sr);
+        }
+
+        // Convert Data Table of strings into double[] inputs and int output
+        // Constructs the Symbol table
+        public void Codify()
+        {
+            codebook = new Codification(Table);
+            Symbols = codebook.Apply(Table);
+            Inputs = Symbols.ToArray(inputColumns);
+            Outputs = Symbols.ToArray<int>(outputColumn);
+        }
+
+        // Loads up the Decision Tree
+        public void CreateDecisionTree()
+        {
+            var attributes = DecisionVariable.FromCodebook(codebook, inputColumns);
+            tree = new DecisionTree(attributes, 6);
+        }
+
+        // Returns Error as a percentage. Best is 0.0, Worst is 1.0
+        public double Learn()
+        {
+            c45 = new C45Learning(tree);
+            return c45.Run(Inputs, Outputs);
+        }
+
+        // Actual Selection using Decision Tree
+        public string GetBestAlgorithmForInput(double[] input)
+        {
+            int result = tree.Compute(input);
+            string bestAlgorithm = codebook.Translate("Selected Sorting Algorithm", result);
+            return bestAlgorithm;
+        }
+
+    }
+}
---
+++ BE Project - Adaptive Sorting Algorithm/Managers/JsonManager.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE_Project___Adaptive_Sorting_Algorithm.Properties;
+using Newtonsoft.Json;
+
+namespace BE_Project___Adaptive_Sorting_Algorithm.Managers
+{
+    class JsonManager
+    {
+        // File names for Array of size 100
+        private static string[] array100 = {
+        Encoding.UTF8.GetString(Resources._100_Random),
+        Encoding.UTF8.GetString(Resources._100_AlmostSorted_1),
+        Encoding.UTF8.GetString(Resources._100_AlmostSorted_2),
+        Encoding.UTF8.GetString(Resources._100_AlmostSorted_3),
+        Encoding.UTF8.GetString(Resources._100_AlmostSorted_4),
+        Encoding.UTF8.GetString(Resources._100_AlmostSorted_5),
+        Encoding.UTF8.GetString(Resources._100_ReverseSorted)
+    };
+        // File names for Array of size 1000 
+        private static string[] array1000 = {
+        Encoding.UTF8.GetString(Resources._1000_Random),
+        Encoding.UTF8.GetString(Resources._1000_AlmostSorted_1),
+        Encoding.UTF8.GetString(Resources._1000_AlmostSorted_2),
+        Encoding.UTF8.GetString(Resources._1000_AlmostSorted_3),
+        Encoding.UTF8.GetString(Resources._1000_AlmostSorted_4),
+        Encoding.UTF8.GetString(Resources._1000_AlmostSorted_5),
+        Encoding.UTF8.GetString(Resources._1000_ReverseSorted)
+    };
+
+        // File names for Array of size 10000 
+        private static string[] array10000 = {
+        Encoding.UTF8.GetString(Resources._10000_Random),
+        Encoding.UTF8.GetString(Resources._10000_AlmostSorted_1),
+        Encoding.UTF8.GetString(Resources._10000_AlmostSorted_2),
+        Encoding.UTF8.GetString(Resources._10000_AlmostSorted_3),
+        Encoding.UTF8.GetString(Resources._10000_AlmostSorted_4),
+        Encoding.UTF8.GetString(Resources._10000_AlmostSorted_5),
+        Encoding.UTF8.GetString(Resources._10000_ReverseSorted)
+    };
+
+        // File names for Array of size 1000000 
+        private static string[] array1000000 = {
+        Encoding.UTF8.GetString(Resources._1000000_Random),
+        Encoding.UTF8.GetString(Resources._1000000_AlmostSorted_1),
+        Encoding.UTF8.GetString(Resources._1000000_AlmostSorted_2),
+        Encoding.UTF8.GetString(Resources._1000000_AlmostSorted_3),
+        Encoding.UTF8.GetString(Resources._1000000_AlmostSorted_4),
+        Encoding.UTF8.GetString(Resources._1000000_AlmostSorted_5),
+        Encoding.UTF8.GetString(Resources._1000000_ReverseSorted)
+     };
+
+
+        // ARRAY TYPE: 
+        public static int TypeRandom = 0; // Not sorted / Randomly arranged
+        public static int TypeAlmostSorted1 = 1; // 10% sorted recurring fragments
+        public static int TypeAlmostSorted2 = 2; // 20% sorted recurring fragments
+        public static int TypeAlmostSorted3 = 3; // 33% sorted recurring fragments
+        public static int TypeAlmostSorted4 = 4; // 40% sorted recurring fragments
+        public static int TypeAlmostSorted5 = 5; // 50% sorted recurring fragments
+        public static int TypeReverseSorted = 6; // Reverse sorted
+        //END of ARRAY TYPES
+
+        public static int Size100 = 100;
+        public static int Size1000 = 1000;
+        public static int Size10000 = 10000;
+        public static int Size1000000 = 1000000;
+
+        // Creates an iterator to load the next result for the given Array filename
+        public static IEnumerable<string> GetNextResult(string filename)
+        {
+            string line;
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+        // Returns the wrapper Result class parsed from the JSON string
+        public static Result ParseResult(string resultString)
+        {
+            BaseResult br = JsonConvert.DeserializeObject<BaseResult>(resultString);
+            Result r = new Result(br);
+            long[] time = new long[6];
+            long min = long.MaxValue;
+            int minIndex = 0;
+
+            time[0] = r.insertionSortExecutionTime;
+            time[1] = r.shellSortExecutionTime;
+            time[2] = r.heapSortExecutionTime;
+            time[3] = r.mergeSortExecutionTime;
+            time[4] = r.quickSortExecutionTime;
+            time[5] = r.parallelMergeSortExecutionTime;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (time[i] < min)
+                {
+                    min = time[i];
+                    minIndex = i;
+                }
+            }
+
+            switch (minIndex)
+            {
+                case 0: r.bestClass = "Insertion Sort";
+                    break;
+                case 1: r.bestClass = "Shell Sort";
+                    break;
+                case 2: r.bestClass = "Heap Sort";
+                    break;
+                case 3: r.bestClass = "Merge Sort";
+                    break;
+                case 4: r.bestClass = "Quick Sort";
+                    break;
+                case 5: r.bestClass = "Parallel Quick Sort";
+                    break;
+            }
+
+            return r;
+        }
+
+        // Helper method to obtain filename of Random array of some size
+        public static string getRandomArrayOfSize(int size)
+        {
+            switch (size)
+            {
+                case 100:
+                    return array100[TypeRandom];
+                case 1000:
+                    return array1000[TypeRandom];
+                case 10000:
+                    return array10000[TypeRandom];
+                case 1000000:
+                    return array1000000[TypeRandom];
+            }
+            return array100[TypeRandom];
+        }
+
+        // Helper method to obtain filename of Reverse Sorted array of some size
+        public static string getReversedArrayOfSize(int size)
+        {
+            switch (size)
+            {
+                case 100:
+                    return array100[TypeReverseSorted];
+                case 1000:
+                    return array1000[TypeReverseSorted];
+                case 10000:
+                    return array10000[TypeReverseSorted];
+                case 1000000:
+                    return array1000000[TypeReverseSorted];
+            }
+            return array100[TypeRandom];
+        }
+
+        // Helper method to obtain filename of Almost Sorted array of some size
+        public static string getAlmostSortedArrayOfSize(int size, int preSortednessDegree)
+        {
+            switch (size)
+            {
+                case 100:
+                    switch (preSortednessDegree)
+                    {
+                        case 1:
+                            return array100[TypeAlmostSorted1];
+                        case 2:
+                            return array100[TypeAlmostSorted2];
+                        case 3:
+                            return array100[TypeAlmostSorted3];
+                        case 4:
+                            return array100[TypeAlmostSorted4];
+                        case 5:
+                            return array100[TypeAlmostSorted5];
+                    }
+                    return array100[TypeAlmostSorted1];
+                case 1000:
+                    switch (preSortednessDegree)
+                    {
+                        case 1:
+                            return array1000[TypeAlmostSorted1];
+                        case 2:
+                            return array1000[TypeAlmostSorted2];
+                        case 3:
+                            return array1000[TypeAlmostSorted3];
+                        case 4:
+                            return array1000[TypeAlmostSorted4];
+                        case 5:
+                            return array1000[TypeAlmostSorted5];
+                    }
+                    return array1000[TypeAlmostSorted1];
+                case 10000:
+                    switch (preSortednessDegree)
+                    {
+                        case 1:
+                            return array10000[TypeAlmostSorted1];
+                        case 2:
+                            return array10000[TypeAlmostSorted2];
+                        case 3:
+                            return array10000[TypeAlmostSorted3];
+                        case 4:
+                            return array10000[TypeAlmostSorted4];
+                        case 5:
+                            return array10000[TypeAlmostSorted5];
+                    }
+                    return array10000[TypeAlmostSorted1];
+                case 1000000:
+                    switch (preSortednessDegree)
+                    {
+                        case 1:
+                            return array1000000[TypeAlmostSorted1];
+                        case 2:
+                            return array1000000[TypeAlmostSorted2];
+                        case 3:
+                            return array1000000[TypeAlmostSorted3];
+                        case 4:
+                            return array1000000[TypeAlmostSorted4];
+                        case 5:
+                            return array1000000[TypeAlmostSorted5];
+                    }
+                    return array1000000[TypeAlmostSorted1];
+            }
+            return array100[TypeAlmostSorted1];
+        }
+
+        // Wrapper class to parse the JSON string into an object
+        public class BaseResult
+        {
+            public int arraySize { get; set; }
+            public double runs { get; set; }
+            public string arraytype { get; set; }
+            public long insertionSortExecutionTime { get; set; }
+            public long shellSortExecutionTime { get; set; }
+            public long heapSortExecutionTime { get; set; }
+            public long mergeSortExecutionTime { get; set; }
+            public long quickSortExecutionTime { get; set; }
+            public long parallelMergeSortExecutionTime { get; set; }
+
+            public BaseResult() { }
+        }
+
+        // Wrapper of BaseResult to hold the calculation of the best algorithm for each array
+        public class Result : BaseResult
+        {
+            public string bestClass;
+
+            public Result(BaseResult br)
+            {
+                this.arraySize = br.arraySize;
+                this.runs = br.runs;
+                this.arraytype = br.arraytype;
+                this.insertionSortExecutionTime = br.insertionSortExecutionTime;
+                this.shellSortExecutionTime = br.shellSortExecutionTime;
+                this.heapSortExecutionTime = br.heapSortExecutionTime;
+                this.mergeSortExecutionTime = br.mergeSortExecutionTime;
+                this.quickSortExecutionTime = br.quickSortExecutionTime;
+                this.parallelMergeSortExecutionTime = br.parallelMergeSortExecutionTime;
+            }
+        }
+    }
+}
